d06df3b1b1fcc749f8ab1294c7e27a2c6d642eee
koppela sumanth <sumanth690@gmail.com>
2018-08-09 10:53:34 +0530

    scenario1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Scenario1Module } from './scenario1/scenario1.module';
-import { Scenario2Module } from './scenario2/scenario2.module';
-import { Scenario3Module } from './scenario3/scenario3.module';
 
 
 @NgModule({
@@ -13,8 +11,6 @@ import { Scenario3Module } from './scenario3/scenario3.module';
   imports: [
     BrowserModule,
     Scenario1Module,
-    Scenario2Module,
-    Scenario3Module
   ],
   providers: [],
   bootstrap: [AppComponent]
